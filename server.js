1e218751630aaa05fc71d9b0a73e35ca558503e2
PAPAHAAAN <insung.han@ubeeslab.com>
2026-09-22 23:33:56 +0900

    feat(teaser4): add SAI teaser 4 high-fidelity landing and review link

--- server.js
+++ server.js
@@ -468,7 +468,7 @@ const server = http.createServer((req, res) => {
   let targetFile;
   if (pathname === '/' || pathname === '/index.html') {
     targetFile = 'teaser-v9.html';
-  } else if (['/teaser2', '/teaser2/', '/teaser3', '/teaser3/', '/teaser-review', '/teaser-review/'].includes(pathname)) {
+  } else if (['/teaser2', '/teaser2/', '/teaser3', '/teaser3/', '/teaser4', '/teaser4/', '/teaser-review', '/teaser-review/'].includes(pathname)) {
     targetFile = pathname.replace(/^\/+|\/+$/g, '') + '.html';
   } else if (pathname === '/favicon.ico') {
     res.statusCode = 200;
